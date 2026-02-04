588fbfdbdb5eb823c6ea20aa9a365e016827198f
Johny Alam <johny.alam@hublet.fi>
2026-02-04 20:56:06 +0200

    Hilt dependency injection added

--- app/src/main/java/fi/developer/designpattern_android/viewmodel/HomeViewModel.kt
+++ app/src/main/java/fi/developer/designpattern_android/viewmodel/HomeViewModel.kt
@@ -1,13 +1,16 @@
 package fi.developer.designpattern_android.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import fi.developer.basic_kotlin.system.DeviceInformation
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class HomeViewModel(application: Application) : AndroidViewModel(application)  {
+@HiltViewModel
+class HomeViewModel @Inject constructor(application: Application) : ViewModel()  {
     private val _deviceInfo = MutableStateFlow(DeviceInformation.from(application))
     val deviceInfo: StateFlow<DeviceInformation> = _deviceInfo.asStateFlow()
 }
